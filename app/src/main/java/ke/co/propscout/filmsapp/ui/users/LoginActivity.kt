1909d8be349bb1a33a6c6e5ff220e97c92c9da05
Azenga Kevin <azenga.kevin7@gmail.com>
2020-08-21 19:11:01 +0300

    Activity navigation handle and enaled data binding in auth activities

--- app/src/main/java/ke/co/propscout/filmsapp/ui/users/LoginActivity.kt
+++ app/src/main/java/ke/co/propscout/filmsapp/ui/users/LoginActivity.kt
@@ -1,12 +1,35 @@
 package ke.co.propscout.filmsapp.ui.users
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import ke.co.propscout.filmsapp.R
+import ke.co.propscout.filmsapp.databinding.ActivityLoginBinding
+import ke.co.propscout.filmsapp.ui.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        //Login Button Click Handler
+        binding.loginButton.setOnClickListener {
+            Intent(this, HomeActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            }.also {
+                startActivity(it)
+            }
+        }
+
+        //Sign Up Text View Click Handler
+        binding.signUpTextView.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+
     }
 }
--- app/src/main/java/ke/co/propscout/filmsapp/ui/users/RegisterActivity.kt
+++ app/src/main/java/ke/co/propscout/filmsapp/ui/users/RegisterActivity.kt
@@ -1,12 +1,36 @@
 package ke.co.propscout.filmsapp.ui.users
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import ke.co.propscout.filmsapp.R
+import ke.co.propscout.filmsapp.databinding.ActivityRegisterBinding
+import ke.co.propscout.filmsapp.ui.HomeActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        //Register Button CLick Handler
+        binding.registerButton.setOnClickListener {
+
+            Intent(this, HomeActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            }.also {
+                startActivity(it)
+            }
+
+        }
+
+        //Sign In Text View CLick Handler
+        binding.signInTextView.setOnClickListener {
+            finish()
+        }
     }
 }
